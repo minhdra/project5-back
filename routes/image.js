f621ae0f478171dd8faaa314be83ab4a4bee7345
Le Minh <le.minh011100@gmail.com>
2023-01-26 17:41:09 +0700

    fixed upload function

--- routes/image.js
+++ routes/image.js
@@ -7,7 +7,7 @@ var verifyToken = require('../middleware/verifyToken');
 var verifyTokenClient = require('../middleware/verifyTokenClient');
 
 /* GET users listing. */
-router.post('/upload', verifyToken, upload.single('file'), ImageController.uploadSingle);
+// router.post('/upload', verifyToken, upload.single('file'), ImageController.uploadSingle);
 router.post('/client-upload', verifyTokenClient, fileUploader.single('file'), ImageController.uploadSingle);
 router.post('/cloudinary-upload', verifyToken, fileUploader.single('file'), ImageController.uploadSingle);
 router.post('/cloudinary-multi-upload', fileUploader.array('files'), ImageController.uploadMultipleCloudinary);
