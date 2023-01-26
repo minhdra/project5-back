adcd50588972f608e17c134f94c294b20a0361db
Le Minh <le.minh011100@gmail.com>
2023-01-26 17:38:44 +0700

    fixed uploadMiddleware

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,32 +1,32 @@
-// uploadMiddleware.js
-const { v4: uuidv4 } = require('uuid');
+// // uploadMiddleware.js
+// const { v4: uuidv4 } = require('uuid');
 
-const multer = require('multer');
+// const multer = require('multer');
 
-// SET STORAGE
-var storage = multer.diskStorage({
-  destination: './public/images',
-  filename: function (req, file, cb) {
-    let ext = '';
-    switch (file.mimetype) {
-      case 'image/jpeg':
-        ext = '.jpeg';
-        break;
-      case 'image/png':
-        ext = '.png';
-        break;
-    }
-    const uniqueSuffix = uuidv4();
-    cb(null, file.fieldname + '-' + uniqueSuffix + ext);
-  },
-});
+// // SET STORAGE
+// var storage = multer.diskStorage({
+//   destination: './public/images',
+//   filename: function (req, file, cb) {
+//     let ext = '';
+//     switch (file.mimetype) {
+//       case 'image/jpeg':
+//         ext = '.jpeg';
+//         break;
+//       case 'image/png':
+//         ext = '.png';
+//         break;
+//     }
+//     const uniqueSuffix = uuidv4();
+//     cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+//   },
+// });
 
-const upload = multer({
-  // limits: {
-  //   fileSize: 4 * 1024 * 1024,
-  // },
-  // dest: "public/images",
-  storage: storage,
-});
+// const upload = multer({
+//   // limits: {
+//   //   fileSize: 4 * 1024 * 1024,
+//   // },
+//   // dest: "public/images",
+//   storage: storage,
+// });
 
-module.exports = upload;
+// module.exports = upload;
